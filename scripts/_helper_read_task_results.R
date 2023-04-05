c0f243a39fc843b4f0fe1b8e56c4c8dece389c5d
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-05 21:59:15 +0200

    update

---
+++ scripts/_helper_read_task_results.R
@@ -0,0 +1,82 @@
+`%|%` <- function(x, y) {
+  ifelse(is.na(x), y, x)
+}
+
+read_task_results <- function(task_path) {
+  task_info <- jsonlite::read_json(paste0(task_path, "/task_info.json"))
+  task_info$task_description <- task_info$task_description %||% NA_character_
+
+  method_info <- jsonlite::read_json(paste0(task_path, "/method_info.json"), simplifyVector = TRUE)
+  metric_info <- jsonlite::read_json(paste0(task_path, "/metric_info.json"), simplifyVector = TRUE)
+  dataset_info <- jsonlite::read_json(paste0(task_path, "/dataset_info.json"), simplifyVector = TRUE)
+  results <- jsonlite::read_json(paste0(task_path, "/results.json"), simplifyVector = TRUE)
+  qc <- jsonlite::read_json(paste0(task_path, "/quality_control.json"), simplifyVector = TRUE)
+
+  for (col in c("method_summary")) {
+    method_info[[col]] <- method_info[[col]] %||% NA_character_
+  }
+  for (col in c("metric_summary")) {
+    metric_info[[col]] <- metric_info[[col]] %||% NA_character_
+  }
+  for (col in c("dataset_summary")) {
+    dataset_info[[col]] <- dataset_info[[col]] %||% NA_character_
+  }
+
+  results_long <-
+    inner_join(
+      results %>%
+        unnest(metric_values) %>%
+        gather(metric_id, value, any_of(metric_info$metric_id)) %>%
+        mutate(value = ifelse(is.na(value), NA_real_, value)) %>%
+        select(method_id, dataset_id, metric_id, value),
+      results %>%
+        unnest(scaled_scores) %>%
+        gather(metric_id, score, any_of(metric_info$metric_id)) %>%
+        mutate(score = ifelse(is.na(score), NA_real_, score)) %>%
+        select(method_id, dataset_id, metric_id, score),
+      by = c("method_id", "dataset_id", "metric_id")
+    ) %>%
+    left_join(method_info %>% select(method_id, is_baseline), "method_id")
+
+  overall_ranking <- results_long %>%
+    group_by(method_id) %>%
+    summarise(mean_score = mean(score %|% 0)) %>%
+    arrange(desc(mean_score))
+
+  # order by ranking
+  results_long$method_id <- factor(results_long$method_id, levels = rev(overall_ranking$method_id))
+  results$method_id <- factor(results$method_id, levels = rev(overall_ranking$method_id))
+  method_info$method_id <- factor(method_info$method_id, levels = rev(overall_ranking$method_id))
+    
+  # create summary statistics
+  overall <- results_long %>%
+    group_by(method_id) %>%
+    summarise(mean_score = mean(score %|% 0), .groups = "drop") %>%
+    mutate(mean_score = pmin(1, pmax(mean_score, 0))) %>%
+    arrange(mean_score)
+  per_dataset <- results_long %>%
+    group_by(method_id, dataset_id) %>%
+    summarise(score = mean(score %|% 0), .groups = "drop") %>%
+    mutate(dataset_id = paste0("dataset_", dataset_id)) %>%
+    mutate(score = pmin(1, pmax(score, 0))) %>%
+    spread(dataset_id, score)
+  per_metric <- results_long %>%
+    group_by(method_id, metric_id) %>%
+    summarise(score = mean(score %|% 0), .groups = "drop") %>%
+    mutate(metric_id = paste0("metric_", metric_id)) %>%
+    mutate(score = pmin(1, pmax(score, 0))) %>%
+    spread(metric_id, score)
+
+  list(
+    task_info = task_info,
+    dataset_info = dataset_info,
+    method_info = method_info,
+    metric_info = metric_info,
+    qc = qc,
+    results = results,
+    results_long = results_long,
+    overall = overall,
+    per_dataset = per_dataset,
+    per_metric = per_metric
+  )
+}
--- scripts/supnote2_figures.R
+++ scripts/supnote2_figures.R
@@ -2,101 +2,29 @@
 library(tidyverse)
 library(funkyheatmap)
 
-task_info_jsons <- list.files("website/results/", pattern = "task_info.json", recursive = TRUE, full.names = TRUE)
+task_info_jsons <- list.files("website/results", pattern = "task_info.json", recursive = TRUE, full.names = TRUE)
+
+source("scripts/_helper_read_task_results.R")
 
 walk(task_info_jsons, function(task_info_json) {
-  data_dir <- dirname(task_info_json)
+  cat("Processing ", basename(dirname(dirname(task_info_json))), "\n", sep = "")
 
   tryCatch({
-    # read task info
-    task_info <- jsonlite::read_json(paste0(data_dir, "/task_info.json"))
-    task_info$task_description <- task_info$task_description %||% NA_character_
-
-    cat("Processing ", task_info$task_id, "\n", sep = "")
+    task_data <- read_task_results(dirname(task_info_json))
 
     figure_path <- paste0("figures/supnote2_figures/", task_info$task_id, "/plot.pdf")
     if (!dir.exists(dirname(figure_path))) {
       dir.create(dirname(figure_path), recursive = TRUE, showWarnings = FALSE)
     }
 
-    `%|%` <- function(x, y) {
-      ifelse(is.na(x), y, x)
-    }
-
-    method_info <- jsonlite::read_json(paste0(data_dir, "/method_info.json"), simplifyVector = TRUE)
-    metric_info <- jsonlite::read_json(paste0(data_dir, "/metric_info.json"), simplifyVector = TRUE)
-    dataset_info <- jsonlite::read_json(paste0(data_dir, "/dataset_info.json"), simplifyVector = TRUE)
-    results <- jsonlite::read_json(paste0(data_dir, "/results.json"), simplifyVector = TRUE)
-    qc <- jsonlite::read_json(paste0(data_dir, "/quality_control.json"), simplifyVector = TRUE)
-
-    for (col in c("method_summary")) {
-      method_info[[col]] <- method_info[[col]] %||% NA_character_
-    }
-    for (col in c("metric_summary")) {
-      metric_info[[col]] <- metric_info[[col]] %||% NA_character_
-    }
-    for (col in c("dataset_summary")) {
-      dataset_info[[col]] <- dataset_info[[col]] %||% NA_character_
-    }
-
-    num_methods <- sum(!method_info$is_baseline)
-    num_baselines <- sum(method_info$is_baseline)
-    num_datasets <- nrow(dataset_info)
-    num_metrics <- nrow(metric_info)
-
-    results_long <-
-      inner_join(
-        results %>%
-          unnest(metric_values) %>%
-          gather(metric_id, value, any_of(metric_info$metric_id)) %>%
-          mutate(value = ifelse(is.na(value), NA_real_, value)) %>%
-          select(method_id, dataset_id, metric_id, value),
-        results %>%
-          unnest(scaled_scores) %>%
-          gather(metric_id, score, any_of(metric_info$metric_id)) %>%
-          mutate(score = ifelse(is.na(score), NA_real_, score)) %>%
-          select(method_id, dataset_id, metric_id, score),
-        by = c("method_id", "dataset_id", "metric_id")
-      ) %>%
-      left_join(method_info %>% select(method_id, is_baseline), "method_id")
-
-    overall_ranking <- results_long %>%
-      group_by(method_id) %>%
-      summarise(mean_score = mean(score %|% 0)) %>%
-      arrange(desc(mean_score))
-
-    # order by ranking
-    results_long$method_id <- factor(results_long$method_id, levels = rev(overall_ranking$method_id))
-    results$method_id <- factor(results$method_id, levels = rev(overall_ranking$method_id))
-    method_info$method_id <- factor(method_info$method_id, levels = rev(overall_ranking$method_id))
-
-
-    overall <- results_long %>%
-      group_by(method_id) %>%
-      summarise(mean_score = mean(score %|% 0), .groups = "drop") %>%
-      mutate(mean_score = pmin(1, pmax(mean_score, 0))) %>%
-      arrange(mean_score)
-    per_dataset <- results_long %>%
-      group_by(method_id, dataset_id) %>%
-      summarise(score = mean(score %|% 0), .groups = "drop") %>%
-      mutate(dataset_id = paste0("dataset_", dataset_id)) %>%
-      mutate(score = pmin(1, pmax(score, 0))) %>%
-      spread(dataset_id, score)
-    per_metric <- results_long %>%
-      group_by(method_id, metric_id) %>%
-      summarise(score = mean(score %|% 0), .groups = "drop") %>%
-      mutate(metric_id = paste0("metric_", metric_id)) %>%
-      mutate(score = pmin(1, pmax(score, 0))) %>%
-      spread(metric_id, score)
-
     summary_all <-
-      method_info %>%
-      filter(!is_baseline) %>%
-      select(method_id, method_name) %>%
-      left_join(overall, by = "method_id") %>%
-      left_join(per_dataset, by = "method_id") %>%
-      left_join(per_metric, by = "method_id") %>%
-      arrange(desc(method_id))
+      task_data$method_info %>%
+        filter(!is_baseline) %>%
+        select(method_id, method_name) %>%
+        left_join(task_data$overall, by = "method_id") %>%
+        left_join(task_data$per_dataset, by = "method_id") %>%
+        left_join(task_data$per_metric, by = "method_id") %>%
+        arrange(desc(method_id))
 
     column_info <-
       bind_rows(
@@ -105,13 +33,13 @@ walk(task_info_jsons, function(task_info_json) {
           "method_name", "Name", "method", "text",
           "mean_score", "Score", "mean", "bar",
         ),
-        dataset_info %>% transmute(
+        task_data$dataset_info %>% transmute(
           id = paste0("dataset_", dataset_id),
           name = dataset_name,
           group = "dataset",
           geom = "funkyrect"
         ),
-        metric_info %>% transmute(
+        task_data$metric_info %>% transmute(
           id = paste0("metric_", metric_id),
           name = metric_name,
           group = "metric",
@@ -136,13 +64,10 @@ walk(task_info_jsons, function(task_info_json) {
     column_groups <- tribble(
       ~Category, ~group, ~palette,
       "", "method", NA_character_,
-      "Overall", "mean", "mean"
+      "Overall", "mean", "mean",
+      "Dataset", "dataset", "dataset",
+      "Metric", "metric", "metric"
     )
-    column_groups <- column_groups %>%
-      add_row(Category = "Dataset", group = "dataset", palette = "dataset")
-
-    column_groups <- column_groups %>%
-      add_row(Category = "Metric", group = "metric", palette = "metric")
 
     palettes <- list(
       mean = "Greys",
